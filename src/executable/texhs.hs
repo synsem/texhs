b89012f57be3d4df874bb4aeed6de2fbc4bff130
Mathias Schenner <mathias.schenner@langsci-press.org>
2015-06-10 14:00:05 +0200

    tex: add simple executable driver

---
+++ src/executable/texhs.hs
@@ -0,0 +1,26 @@
+----------------------------------------------------------------------
+-- |
+-- Module      :  Main
+-- Copyright   :  (c) Mathias Schenner 2015,
+--                (c) Language Science Press 2015.
+-- License     :  GPL-3
+--
+-- Maintainer  :  mathias.schenner@langsci-press.org
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Executable driver for texhs.
+----------------------------------------------------------------------
+
+module Main where
+
+import System.Environment
+
+import Text.TeX (readTeX)
+
+main :: IO ()
+main = do
+  argv <- getArgs
+  case argv of
+    [filename] -> print . readTeX filename =<< readFile filename
+    _ -> error "Usage: provide a texfile as single argument"
