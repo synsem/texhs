3419543dbd0b576e789f09ba46c8d62ef5afe191
Mathias Schenner <mathias.schenner@langsci-press.org>
2015-11-18 15:25:29 +0100

    cli: add basic cmdline options

--- src/executable/texhs.hs
+++ src/executable/texhs.hs
@@ -14,16 +14,119 @@
 
 module Main where
 
+import Data.Char (toLower)
+import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy.IO as T
-import System.Environment
+import System.Console.GetOpt
+import System.Environment (getArgs, getProgName)
 
 import Text.TeX (readTeXIO)
-import Text.Doc (tex2doc, doc2html)
+import Text.Doc (Doc, tex2doc, doc2xml, doc2html)
+
+
+---------- options
+
+data OutputFormat
+  = XML
+  | HTML
+  | InvalidFormat String
+  deriving (Eq, Show)
+
+data Options = Options
+  { optShowUsage    :: Bool
+  , optShowVersion  :: Bool
+  , optVerbose      :: Bool
+  , optOutputFormat :: OutputFormat
+  , optOutputFile   :: Maybe FilePath
+  } deriving Show
+
+defaultOptions :: Options
+defaultOptions = Options
+  { optShowUsage    = False
+  , optShowVersion  = False
+  , optVerbose      = False
+  , optOutputFormat = HTML
+  , optOutputFile   = Nothing
+  }
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['h'] ["help"]
+    (NoArg (\ opts -> opts { optShowUsage = True }))
+    "show usage information"
+  , Option ['V'] ["version"]
+    (NoArg (\ opts -> opts { optShowVersion = True }))
+    "show version number"
+  , Option ['v'] ["verbose"]
+    (NoArg (\ opts -> opts { optVerbose = True }))
+    "verbose output on stderr"
+  , Option ['t'] ["target"]
+    (ReqArg ((\ f opts -> opts { optOutputFormat = parseOutputFormat f })) "FMT")
+    "output format (xml, html)"
+  , Option ['o'] ["output"]
+    (ReqArg (\ f opts -> opts { optOutputFile = Just f }) "FILE")
+    "output file"
+  ]
+
+parseOutputFormat :: String -> OutputFormat
+parseOutputFormat xs =
+  case map toLower xs of
+    "xml" -> XML
+    "html" -> HTML
+    _ -> InvalidFormat xs
+
+parseOptions :: String -> [String] -> IO (Options, [String])
+parseOptions name argv =
+  case getOpt Permute options argv of
+    (opts, args, []) -> return (foldl (flip id) defaultOptions opts, args)
+    (_, _, errs) -> error $ concat errs ++ usageInfo (usageHeader name) options
+
+usageHeader :: String -> String
+usageHeader name = "Usage: " ++ name ++ " [options] texfile"
+
+
+---------- conversion
+
+runConversion :: Options -> String -> IO ()
+runConversion opts filename =
+  case optOutputFormat opts of
+    HTML -> toHtml filename >>= output opts
+    XML -> toXml filename >>= output opts
+    InvalidFormat fmt -> error $ "invalid or unsupported format: " ++ fmt
+
+parseDoc :: FilePath -> IO Doc
+parseDoc filename =
+  tex2doc filename <$> (readTeXIO filename =<< readFile filename)
+
+toHtml :: FilePath -> IO Text
+toHtml filename = doc2html <$> parseDoc filename
+
+toXml :: FilePath -> IO Text
+toXml filename = doc2xml <$> parseDoc filename
+
+output :: Options -> Text -> IO ()
+output opts = maybe T.putStrLn T.writeFile (optOutputFile opts)
+
+
+---------- main
+
+showVersion :: String -> IO ()
+showVersion name = putStrLn $ name ++ ": pre-release version"
+
+showUsage :: String -> IO ()
+showUsage name = error $ usageInfo (usageHeader name) options
+
+run :: String -> Options -> [String] -> IO ()
+run name opts args
+  | optShowUsage opts = showUsage name
+  | optShowVersion opts = showVersion name
+  | otherwise = case args of
+    [filename] -> runConversion opts filename
+    _ -> error "usage error: expecting texfile as single argument"
 
 main :: IO ()
 main = do
+  name <- getProgName
   argv <- getArgs
-  case argv of
-    [filename] -> T.putStrLn =<< (doc2html `fmap` tex2doc filename `fmap`
-                                  (readTeXIO filename =<< readFile filename))
-    _ -> error "Usage: provide a texfile as single argument"
+  (opts, args) <- parseOptions name argv
+  run name opts args
--- src/library/Text/Doc.hs
+++ src/library/Text/Doc.hs
@@ -13,14 +13,17 @@
 ----------------------------------------------------------------------
 
 module Text.Doc
-  ( -- * TeX Reader
-    tex2doc
+  ( -- * Doc Type
+    Doc
+    -- * TeX Reader
+  , tex2doc
     -- * HTML Writer
   , doc2html
     -- * XML Writer
   , doc2xml
   ) where
 
+import Text.Doc.Types (Doc)
 import Text.Doc.Reader.TeX (tex2doc)
 import Text.Doc.Writer.Html (doc2html)
 import Text.Doc.Writer.Xml (doc2xml)
