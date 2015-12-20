983c37187d44f3f884e9989e8cf3867e5a659faf
Mathias Schenner <mathias.schenner@langsci-press.org>
2015-12-20 17:41:32 +0100

    model: distribute CiteDB infos to inlines

--- src/library/Text/Doc/Filter/Bib.hs
+++ src/library/Text/Doc/Filter/Bib.hs
@@ -15,15 +15,23 @@
 
 module Text.Doc.Filter.Bib
  ( -- * Bib Filter
+   -- ** Meta
    docBibFilter
+   -- ** Content
+ , distributeMeta
  ) where
 
+import Control.Monad.Trans.Reader
+import Data.List (intercalate)
 import qualified Data.Map.Strict as M
+import Data.Maybe (mapMaybe)
 
 import Text.Bib.Types (BibDB)
-import Text.Bib.Writer (resolveCitations)
+import Text.Bib.Writer (resolveCitations, fmtCiteEntries)
 import Text.Doc.Types
 
+
+-------------------- Meta
 
 -- | Resolve bibliographic references in a document ('Doc')
 -- relative to a bibliographic database ('BibDB').
@@ -41,3 +49,55 @@ populateCiteDB db meta =
   let keys = M.keys (metaCiteMap meta)
       citeDB = resolveCitations db keys
   in meta { metaCiteDB = citeDB }
+
+
+-------------------- Content
+
+-- NOTE: This processing step is intended to help writers
+-- that cannot easily run on top of a Reader monad themselves
+-- and thus have no online access to document meta information
+-- (e.g. writers based on blaze-markup or blaze-html).
+
+-- | Distribute document information from Meta to content elements
+-- (blocks and inlines).
+--
+-- In particular, inject formatted versions of citations into the
+-- second argument of 'Citation' inlines.
+distributeMeta :: Doc -> Doc
+distributeMeta (Doc meta content) =
+  Doc meta (runReader (distributeMetaToContent content) meta)
+
+distributeMetaToContent :: Content -> Reader Meta Content
+distributeMetaToContent content =
+  mapM distributeMetaToBlock content
+
+distributeMetaToBlock :: Block -> Reader Meta Block
+distributeMetaToBlock (Para xs) =
+  Para <$> mapM distributeMetaToInline xs
+distributeMetaToBlock (Header l xs) =
+  Header l <$> mapM distributeMetaToInline xs
+distributeMetaToBlock (List xss) =
+  List <$> mapM (mapM distributeMetaToBlock) xss
+
+distributeMetaToInline :: Inline -> Reader Meta Inline
+distributeMetaToInline (Str xs) =
+  return $ Str xs
+distributeMetaToInline (Normal xs) =
+  Normal <$> mapM distributeMetaToInline xs
+distributeMetaToInline (Emph xs) =
+  Emph <$> mapM distributeMetaToInline xs
+distributeMetaToInline Space =
+  return $ Space
+distributeMetaToInline (Citation cit _) =
+  asks metaCiteDB >>= \ db ->
+  return $ Citation cit (Just (fmtMultiCite db cit))
+
+-- Stub. Ignores citation mode and pre-/post-notes.
+fmtMultiCite :: CiteDB -> MultiCite -> [Inline]
+fmtMultiCite db (MultiCite _ _ _ xs) =
+  intercalate [Str ";", Space] (map (fmtSingleCite db) xs)
+
+-- Stub. Ignores pre-/post-notes.
+fmtSingleCite :: CiteDB -> SingleCite -> [Inline]
+fmtSingleCite db (SingleCite _ _ keys) =
+  fmtCiteEntries (mapMaybe (flip M.lookup db) keys)
